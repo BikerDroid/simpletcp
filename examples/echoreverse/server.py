f9eb04c8b13a6e9c90bcf2d1d3e3fc953a3f9fcc
Thomas David Fischer <tdf.tomfischer@gmail.com>
2016-01-28 13:40:59 -0600

    Update echoreverse example: Add IP argument to callback function. Explain reverse process with comments.

--- examples/echoreverse/server.py
+++ examples/echoreverse/server.py
@@ -1,8 +1,17 @@
 import queue
 from simpletcp.tcpserver import TCPServer
 
-def echoreverse(queue, data):
-    queue.put(bytes("".join([chr(b) for b in list(reversed(data))]), "UTF-8"))
+def echoreverse(ip, queue, data):
+    # Reverse the data.
+    # result = reversed(data)
+    # Convert each byte to a character.
+    # result = [chr(b) for b in reversed(data)]
+    # Turn the list of characters into a string with "" in between each char.
+    # result = "".join([chr(b) for b in reversed(data)])
+    # Turn the whole thing back into bytes with UTF-8 encoding.
+    # result = bytes("".join([chr(b) for b in reversed(data)]), "UTF-8")
+    # Put the result back into the queue of data to be sent back:
+    queue.put(bytes("".join([chr(b) for b in reversed(data)]), "UTF-8"))
 
 server = TCPServer("localhost", 5000, echoreverse)
 server.run()
